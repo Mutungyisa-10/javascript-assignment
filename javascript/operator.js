461fa1e924c13a2dcb42d12682891be515ad840a
Mutungyisa-10 <98768637+Mutungyisa-10@users.noreply.github.com>
2022-02-13 12:27:58 +0300

    Update operator.js
    I have tried that which i think if right should be added

--- javascript/operator.js
+++ javascript/operator.js
@@ -1,45 +1,45 @@
 let x = 5;
 let y = 3;
 
-//
+//this means 5 plus 3 which will give the answer in the terminal
 console.log('x + y = ', x + y);  //
 
-//
+//this implements 5 minus 3 which will give the answer when run in the terminal
 console.log('x - y = ', x - y);  //
 
-//
+//this means x*y which is 5 times 3 which gives the answer when run the terminal
 console.log('x * y = ', x * y);  //
 
-//
+//this means 5 divide by 3 should gives the answer when run in the terminal
 console.log('x / y = ', x / y);  //
-//
+//this mean 5 modulus 3 where there reminder is taken as the answer after dividing should give the answer when run in terminal
 console.log('x % y = ', x % y);   //
 
-// increment-
-console.log('++x = ', ++x); //
-console.log('x++ = ', x++); //
-console.log('x = ', x);     //
+// increment- they should give this when run in the terminal
+console.log('++x = ', ++x); //this means that one must be added when 5 is going to using it again
+console.log('x++ = ', x++); //this means that 5 plus 1
+console.log('x = ', x);     //this means that 5=
 
-//
-console.log('--x = ', --x); //
-console.log('x-- = ', x--); //
-console.log('x = ', x);     //
+//it should give this when run in the terminal
+console.log('--x = ', --x); //this means that one must be removed on 5 when it is going to be used again
+console.log('x-- = ', x--); //this means that 5 minus 1
+console.log('x = ', x);     //this means x is x
 
-//
+//this means that x power y when run in the terminal
 console.log('x ** y =', x ** y);
 
-//
-console.log(2 == 2); //
-console.log(2 == '2'); //
+// 2 when run in the terminal
+console.log(2 == 2); //this means that 2 is equal to 2
+console.log(2 == '2'); //this is false because 2 is not equal to '2'
 
-//
-console.log(3 != 2); //
-console.log('hello' != 'Hello'); //
+//Not equal equations when run in the terminal 
+console.log(3 != 2); //this means that 3 is not equal to 2 which is true
+console.log('hello' != 'Hello'); //this mean hello is not equal to Hello which is true
 
-//
-console.log(2 === 2); //
+//Equal equations when run in the terminal
+console.log(2 === 2); //true
 console.log(2 === '2'); // false
 
-//
+//this means it is true if 2 is not equal to '2' and it is false if 2 is not equal to 2 when u run in the terminal
 console.log(2 !== '2'); // true
 console.log(2 !== 2); // false
